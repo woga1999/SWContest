d9b033ce515ffd159e4d823cb52e3f989995cbfb
DESKTOP-G7653J7\Jangwon <wlckd1031@sju.ac.kr>
2017-07-30 02:56:18 +0900

    DialogActivity 삭제, Loading에서 바로 Menu로 이동. SearchActivity생성

---
+++ Navigation/app/src/main/java/com/example/woga1/navigation/MenuActivity.java
@@ -0,0 +1,29 @@
+package com.example.woga1.navigation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+
+public class MenuActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_menu);
+        EditText search = (EditText) findViewById(R.id.search);
+        search.setOnClickListener(new EditText.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuActivity.this, SearchActivity.class));
+            }
+
+        });
+    }
+
+
+}
---
+++ Navigation/app/src/main/java/com/example/woga1/navigation/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.example.woga1.navigation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_search);
+    }
+
+
+}
