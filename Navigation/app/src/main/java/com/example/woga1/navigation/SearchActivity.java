bfcaa09ed7cd393d9d159d167db24b7d8acbe7a5
DESKTOP-G7653J7\Jangwon <wlckd1031@sju.ac.kr>
2017-08-10 21:30:45 +0900

    구글 툴바로 검색한 상세주소 바로 ReadyActivity로 연동. 그 주소로 내비게이션 길찾기 가능. 이 전 commit에서 version25로 낮췄다.

--- Navigation/app/src/main/java/com/example/woga1/navigation/SearchActivity.java
+++ Navigation/app/src/main/java/com/example/woga1/navigation/SearchActivity.java
@@ -2,6 +2,8 @@ package com.example.woga1.navigation;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -27,6 +29,9 @@ import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
+import java.io.IOException;
+import java.util.List;
+
 import static com.example.woga1.navigation.MenuActivity.names;
 
 
@@ -38,7 +43,8 @@ public class SearchActivity extends AppCompatActivity implements PlaceSelectionL
     private static final int REQUEST_SELECT_PLACE = 1000;
     private TextView locationTextView;
     private TextView attributionsTextView;
-
+    private String longtitude;
+    private String latitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -137,6 +143,14 @@ public class SearchActivity extends AppCompatActivity implements PlaceSelectionL
 //        locationTextView.setText(getString(R.string.formatted_place_data, place
 //                .getName(), place.getAddress(), place.getPhoneNumber(), place
 //                .getWebsiteUri(), place.getRating(), place.getId()));
+        changeToLongitudeLatitude(place.getAddress());
+//                changeToLongitudeLatitude("서울 영등포구 도림로53길 9");
+        Intent intent = new Intent(SearchActivity.this, ReadyActivity.class);
+        intent.putExtra("destination", place.getName());
+        intent.putExtra("longtitude",longtitude);
+        intent.putExtra("latitude",latitude);
+        startActivityForResult(intent, 1);
+
         if (!TextUtils.isEmpty(place.getAttributions())){
             attributionsTextView.setText(Html.fromHtml(place.getAttributions().toString()));
         }
@@ -162,4 +176,49 @@ public class SearchActivity extends AppCompatActivity implements PlaceSelectionL
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    private void changeToLongitudeLatitude(CharSequence destinations)
+    {
+        final Geocoder geocoder = new Geocoder(this);
+        List<Address> list = null;
+        List<Address> list1 = null;
+        String start = "세종대학교";
+        String destination = destinations.toString();
+        try {
+            list = geocoder.getFromLocationName(
+                    start, // 지역 이름
+                    10); // 읽을 개수
+            list1 = geocoder.getFromLocationName(destination, 10);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("test","입출력 오류 - 서버에서 주소변환시 에러발생");
+        }
+
+        if (list != null || list1 !=null) {
+            if (list.size() == 0) {
+                Toast.makeText(getApplicationContext(),"해당되는 주소 정보는 없습니다", Toast.LENGTH_LONG).show();
+                //tv.setText("해당되는 주소 정보는 없습니다");
+            }
+            else if(list1.size() ==0) {
+                Toast.makeText(getApplicationContext(),"해당되는 주소 정보는 없습니다", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                Address addr = list.get(0);
+                double startLat = addr.getLatitude();
+                double startLon = addr.getLongitude();
+                Address addr1 = list1.get(0);
+                double endLat = addr1.getLatitude();
+                double endLon = addr1.getLongitude();
+
+
+                latitude= String.valueOf(endLat);
+                longtitude= String.valueOf(endLon);
+//                Toast.makeText(getApplicationContext(),"start- 위도: "+String.valueOf(startLat)+" 경도: "+String.valueOf(startLon)+"  end- 위도:"+String.valueOf(endLat)+" 경도: "+String.valueOf(endLon), Toast.LENGTH_LONG).show();
+                //tv.setText(list.get(0).toString());
+                //          list.get(0).getCountryName();  // 국가명
+                //          list.get(0).getLatitude();        // 위도
+                //          list.get(0).getLongitude();    // 경도
+            }
+        }
+    }
 }
