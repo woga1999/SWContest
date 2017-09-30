4d6bba428c93a5478393c07ffaee7e2315a2706c
박효완 <qkrgy1206@naver.com>
2017-10-01 06:33:26 +0900

    bring data and display POI using ListView

--- Navigation/app/src/main/java/com/example/woga1/navigation/NearActivity.java
+++ Navigation/app/src/main/java/com/example/woga1/navigation/NearActivity.java
@@ -2,11 +2,13 @@ package com.example.woga1.navigation;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,13 +20,9 @@ import com.skp.Tmap.TMapData;
 import com.skp.Tmap.TMapGpsManager;
 import com.skp.Tmap.TMapPOIItem;
 import com.skp.Tmap.TMapPoint;
+import com.skp.Tmap.TMapTapi;
 
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
 import java.util.ArrayList;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 public class NearActivity extends AppCompatActivity {
     //주변검색 할 때 나오는 Activity(주유소 등등) poi
@@ -33,6 +31,9 @@ public class NearActivity extends AppCompatActivity {
     ArrayAdapter adapter;
     TMapGpsManager tMapGpsManager;
     ArrayList<String> POIName = new ArrayList<>();
+    ArrayList<TMapPOIItem> poiItems = new ArrayList<>();
+    int count = 0;
+    String API_KEY = "cad2cc9b-a3d5-3c32-8709-23279b7247f9";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +42,22 @@ public class NearActivity extends AppCompatActivity {
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFFFFFFFF));
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.custom_nearbar);
-        tMapGpsManager = new TMapGpsManager(this);
-        tMapGpsManager.setProvider(TMapGpsManager.NETWORK_PROVIDER);
-        tMapGpsManager.OpenGps();
 
+        TMapTapi tmaptapi = new TMapTapi(this);
+        tmaptapi.setSKPMapAuthentication(API_KEY);
         Button LPG = (Button) findViewById(R.id.btnLPG);
         Button Food = (Button) findViewById(R.id.btnFoodStore);
         Button Mart = (Button) findViewById(R.id.btnMart);
         ImageButton backImageButton = (ImageButton) findViewById(R.id.backimageButton);
         listviewPOI = (ListView) findViewById(R.id.poiListView);
         listviewPOI.setAdapter(adapter);
+        listviewPOI.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               TMapPoint point = poiItems.get(position).getPOIPoint();
+                Toast.makeText(getApplicationContext(), "lat: "+point.getLatitude()+" lon: "+point.getLongitude(), Toast.LENGTH_SHORT).show();
+            }
+        });
         backImageButton.setOnClickListener(new EditText.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -61,18 +68,7 @@ public class NearActivity extends AppCompatActivity {
         LPG.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                searchAroundPOI();
-                                // 해당 작업을 처리함
-                            }
-                        });
-                    }
-                }).start();
+                searchAroundPOI("주유소");
             }
 
         });
@@ -80,91 +76,64 @@ public class NearActivity extends AppCompatActivity {
         Food.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                TMapPoint tmappoint = new TMapPoint(37.550498, 127.073182);
-//                tmapData.findAroundNamePOI(tmappoint, "편의점", new TMapData.FindAroundNamePOIListenerCallback()
-//                {
-//                    @Override
-//                    public void onFindAroundNamePOI(ArrayList<TMapPOIItem> poiItem) {
-//                        for(int i = 0; i < poiItem.size(); i++) {
-//                            TMapPOIItem  item = poiItem.get(i);
-//                            POIName.add(i, item.getPOIName().toString());
-//                            Log.e("NearActivity POIName","POI Name: " + item.getPOIName().toString() + ", " +
-//                                    "Address: " + item.getPOIAddress().replace("null", "")  +
-//                                    ", " + "Point: " + item.getPOIPoint().toString());
-//                        }
-//                    }
-//                });
-//
-//                    adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, POIName) ;
-//                    listviewPOI.setAdapter(adapter);
-
+                searchAroundPOI("음식");
             }
 
         });
         Mart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"클릭", Toast.LENGTH_SHORT).show();
+                searchAroundPOI("편의점");
             }
         });
     }
 
-    public void searchAroundPOI()
+    public void searchAroundPOI(final String category)
     {
         TMapPoint tmappoint = new TMapPoint(37.550498, 127.073182);
-        tmapData.findAroundNamePOI(tmappoint, "주유소", new TMapData.FindAroundNamePOIListenerCallback()
+        POIName.clear();
+        poiItems.clear();
+        if(count != 0) {
+            adapter.clear();
+        }
+        tmapData.findAroundNamePOI(tmappoint, category, new TMapData.FindAroundNamePOIListenerCallback()
         {
             @Override
             public void onFindAroundNamePOI(ArrayList<TMapPOIItem> poiItem) {
                 for(int i = 0; i < poiItem.size(); i++) {
                     TMapPOIItem  item = poiItem.get(i);
                     POIName.add(i, item.getPOIName().toString());
-                    Log.e("NearActivity POIName","POI Name: " + item.getPOIName().toString() + ", " +
+                    poiItems.add(poiItem.get(i));
+                    Log.e("NearActivity"+category,"POI Name: " + item.getPOIName().toString() + ", " +
                             "Address: " + item.getPOIAddress().replace("null", "")  +
                             ", " + "Point: " + item.getPOIPoint().toString());
-                    //adapter.add(POIName);
                 }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, POIName) ;
+
+                                listviewPOI.setAdapter(adapter);
+                                count++;
+                                adapter.notifyDataSetChanged();
+                                // 해당 작업을 처리함
+                            }
+                        });
+                    }
+                }).start();
 
             }
         });
 
     }
+    public class ParsePOI extends AsyncTask<String, Integer, String>{
 
-    public void btnLPG(View v){
-        TMapPoint tmappoint = new TMapPoint(37.550498, 127.073182);
-        try {
-            ArrayList<TMapPOIItem> tMapPOIItems =  tmapData.findAroundKeywordPOI (tmappoint, "주유소", 1, 20);
-            for(int i=0; i< tMapPOIItems.size(); i++)
-            {
-                POIName.add(i, tMapPOIItems.toString());
-            }
-            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, POIName) ;
-            listviewPOI.setAdapter(adapter);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
         }
-    }
-
-    public void btnFoodStore(View v){
-        TMapPoint tmappoint = new TMapPoint(37.550498, 127.073182);
-        try {
-            ArrayList<TMapPOIItem> tMapPOIItems =  tmapData.findAroundKeywordPOI (tmappoint, "음식점", 1, 20);
-            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tMapPOIItems) ;
-            listviewPOI.setAdapter(adapter);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void btnMart(View v){
-        Toast.makeText(getApplicationContext(),"클릭", Toast.LENGTH_SHORT).show();
     }
 }
